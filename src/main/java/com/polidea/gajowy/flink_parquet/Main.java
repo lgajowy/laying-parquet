3048be4e05f753b372a93a1501cbf54020c9f0b3
Łukasz Gajowy <lukasz.gajowy@polidea.com>
2018-06-13 20:23:02 +0200

    Add working parquet write

---
+++ src/main/java/com/polidea/gajowy/flink_parquet/Main.java
@@ -0,0 +1,70 @@
+package com.polidea.gajowy.flink_parquet;
+
+import com.polidea.gajowy.flink_parquet.avro.Record;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.hadoop.mapreduce.HadoopOutputFormat;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.typeutils.TupleTypeInfo;
+import org.apache.flink.api.java.typeutils.TypeExtractor;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.parquet.avro.AvroParquetOutputFormat;
+import org.apache.parquet.hadoop.ParquetOutputFormat;
+import org.apache.parquet.hadoop.metadata.CompressionCodecName;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+    Record sampleRecord = generateSampleObject();
+
+    DataSet<Tuple2<Void, Record>> output = putObjectIntoDataSet(env, sampleRecord);
+
+    writeAvro(output, "file:///Users/lukasz/flink/parquet");
+
+    env.execute();
+  }
+
+  private static void writeAvro(DataSet<Tuple2<Void, Record>> data, String outputPath)
+    throws IOException {
+    Job job = Job.getInstance();
+
+    // Set up Hadoop Output Format
+    HadoopOutputFormat<Void, Record> hadoopOutputFormat
+      = new HadoopOutputFormat<>(new AvroParquetOutputFormat<Record>(), job);
+
+    FileOutputFormat.setOutputPath(job, new Path(outputPath));
+
+    AvroParquetOutputFormat.setSchema(job, Record.getClassSchema());
+    ParquetOutputFormat.setCompression(job, CompressionCodecName.SNAPPY);
+    ParquetOutputFormat.setEnableDictionary(job, true);
+
+    data.output(hadoopOutputFormat);
+  }
+
+  private static Record generateSampleObject() {
+    Record record = new Record();
+    record.setRow("Example Row");
+    return record;
+  }
+
+  private static DataSet<Tuple2<Void, Record>> putObjectIntoDataSet(ExecutionEnvironment env,
+    Record person) {
+    List<Tuple2<Void, Record>> l = Arrays.asList(new Tuple2<Void, Record>(null, person));
+    TypeInformation<Tuple2<Void, Record>> typeInfo
+      = new TupleTypeInfo<>(TypeExtractor.getForClass(Void.class),
+      TypeExtractor.getForClass(Record.class));
+
+    DataSet<Tuple2<Void, Record>> data = env.fromCollection(l, typeInfo);
+
+    return data;
+  }
+}
